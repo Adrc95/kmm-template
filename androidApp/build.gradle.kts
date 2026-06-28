ec03b4b58146f92bb2c7cf24bcea9c71d4fad239
Adrian <awinf204@gmail.com>
2026-06-28 22:52:23 +0200

    Configure JVM target and move dependencies into Kotlin block

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,7 +1,28 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget(BuildVersion.environment.jvmTarget)
+    }
+    dependencies {
+        implementation(projects.shared.presentation.ui)
+        implementation(projects.shared.core.common)
+        implementation(project.dependencies.platform(libs.compose.bom))
+        implementation(libs.compose.foundation)
+        implementation(libs.compose.ui)
+        implementation(libs.compose.runtime.android)
+        implementation(libs.compose.material3)
+        implementation(libs.compose.multiplatform.ui.tooling.preview)
+        implementation(libs.androidx.activityCompose)
+        implementation(libs.bundles.android.core)
+        debugImplementation(libs.compose.uitooling)
+    }
 }
 
 android {
@@ -34,20 +55,6 @@ android {
 
 configurations.configureEach {
     exclude(group = "androidx.constraintlayout", module = "constraintlayout-core")
-}
-
-dependencies {
-    implementation(projects.shared.presentation.ui)
-    implementation(projects.shared.core.common)
-    implementation(project.dependencies.platform(libs.compose.bom))
-    implementation(libs.compose.foundation)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.runtime.android)
-    implementation(libs.compose.material3)
-    implementation(libs.compose.multiplatform.ui.tooling.preview)
-    implementation(libs.androidx.activityCompose)
-    implementation(libs.bundles.android.core)
-    debugImplementation(libs.compose.uitooling)
 }
 
 tasks.register("testClasses") {
